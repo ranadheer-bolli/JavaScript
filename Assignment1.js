41df4fca1dff15a3c42f0898be0ae41475a65ef2
ranadheer-bolli <ranadheer.bolli@zemosolabs.com>
2021-09-21 10:15:20 +0530

    Js Assignment 1

---
+++ Assignment1.js
@@ -0,0 +1,10 @@
+var capitalize = (name)=>{
+    return name.toUpperCase();
+}
+
+var greeting = (name)=>{
+    console.log('HELLO '+name);
+}
+
+greeting(capitalize("ranadheer"))
+
---
+++ Assignment2.js
@@ -0,0 +1,7 @@
+var codeName = (firstName,lastName)=>{
+    return firstName[0]+lastName[0];
+}
+
+console.log(codeName('Roronoa','Zoro'));
+console.log(codeName('Vinsmoke','Sanji'));
+console.log(codeName('Trafalgar','Law'));
